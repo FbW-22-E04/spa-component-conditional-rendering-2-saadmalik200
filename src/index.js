171df9430e9e7dac83aa0965079d37b9256b43f8
Saad Malik <saad_malik200@hotmail.com>
2022-10-11 14:17:39 +0200

    ex6

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import ReactDOM from "react-dom/client";
 
 // add the green inside this function
-const Alert = () => {
+const Alert = (props) => {
+  //your component here
 
-
+  if (props.color === "red") {
+    return (
+      <div className="alert alert-danger" role="alert">
+        {props.text}
+      </div>
+    );
+  } else if (props.color === "orange") {
+    return (
+      <div className="alert alert-warning" role="alert">
+        {props.text}
+      </div>
+    );
+  } else if (props.color === "green") {
+    return (
+      <div className="alert alert-success" role="alert">
+        {props.text}
+      </div>
+    );
+  } else return null;
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<div>
-		<Alert text="OMG! Something really bad has happended!" color="red" />
-		<Alert text="Well, it is not that bad after all!" color="orange" />
-		<Alert text="I am supposed to be green" color="green" />
-	</div>
+  <div>
+    <Alert text="OMG! Something really bad has happended!" color="red" />
+    <Alert text="Well, it is not that bad after all!" color="orange" />
+    <Alert text="I am supposed to be green" color="green" />
+  </div>
 );
